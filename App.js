430f863189caa248621e1958df38825728484efb
julha2005 <134710006+julha2005@users.noreply.github.com>
2023-05-26 11:56:23 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Title from './src/components/title';
-import Form from './src/components/form';
+import Form from './src/Form';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Title/>
+
       <Form/>
+      
       <StatusBar style="auto" />
     </View>
   );
